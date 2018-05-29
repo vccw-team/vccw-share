7a0e315874a792817d76c5393e070176c1108a16
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-05-29 10:17:49 +0900

    bug fix

--- setup.sh
+++ setup.sh
@@ -8,14 +8,17 @@ curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ngrok.
 unzip -u ngrok.zip
 sudo mv ngrok /usr/local/bin/
 
-rm -fr $HOME/proxy
+rm -fr $HOME/vccw-proxy
 git clone https://github.com/vccw-team/vccw-share.git $HOME/vccw-proxy
-cd $HOME/proxy && npm install
+cd $HOME/vccw-proxy && npm install
+
+ADDR=$(ifconfig eth1 | awk '/inet / {print $2}' | awk -F: '{print $2}')
 
 mkdir -p $HOME/.ngrok2
 touch $HOME/.ngrok2/ngrok.yml
 cat << EOS > $HOME/.ngrok2/ngrok.yml
 region: us
+web_addr: ${ADDR}:4040
 tunnels:
   wp:
     proto: http
